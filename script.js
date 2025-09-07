a88232ae37017f89504061ad751090e8966a5059
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-09-07 18:12:42 +0800

    added modal for each tree

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 // all plants
-const allPlants = ()=>{
-    const url = "https://openapi.programming-hero.com/api/plants";
-    fetch(url)
-    .then(res => res.json())
-    .then(data => showAllPlants(data.plants))
-}
-const showAllPlants = (plants)=>{
-    const plantsContainer = document.getElementById("plants-container");
-    plantsContainer.innerHTML="";
+const allPlants = () => {
+  const url = "https://openapi.programming-hero.com/api/plants";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAllPlants(data.plants));
+};
+const showAllPlants = (plants) => {
+  const plantsContainer = document.getElementById("plants-container");
+  plantsContainer.innerHTML = "";
 
-    plants.forEach(plant =>{
-        const div = document.createElement('div');
-        div.innerHTML = `<div class="p-4 flex flex-col justify-around items-start space-y-5 bg-white rounded-lg h-full">
+  plants.forEach((plant) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="p-4 flex flex-col justify-around items-start space-y-5 bg-white rounded-lg h-full">
                      <div  class="w-[320px] h-[186px] rounded-md bg-[url(${plant.image})] bg-cover bg-center"></div>
-                    <h2 class="text-[14px] font-bold">${plant.name}</h2>
+                    <h2 onclick="loadPlantDetails(${plant.id})" class="text-[14px] cursor-pointer font-bold">${plant.name}</h2>
                     <p class="text-[12px]">${plant.description}</p>
                     <div class="text-[14px] flex flex-row justify-between w-full h-[28px] items-center">
                         <button class="btn bg-[#DCFCE7] text-[#15803D] rounded-3xl">${plant.category}</button>
                         <p class="font-bold">৳${plant.price}</p>
                     </div>
                     <button class="btn text-white bg-[#15803D] w-full">Add to Cart</button>
-                </div>`
-        plantsContainer.appendChild(div);
-    })
-}
+                </div>`;
+    plantsContainer.appendChild(div);
+  });
+};
 allPlants();
-
 
 // all categories
 const allCategories = () => {
-    const url = "https://openapi.programming-hero.com/api/categories";
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => showAllCategory(data.categories));
-  };
-  
-  const showAllCategory = (categories) => {
-    const categoryContainer = document.getElementById("category-container");
-    categoryContainer.innerHTML = " ";
-    categoryContainer.innerHTML = '<button onclick="allPlants()" class="h-[40px] py-2 px-4 rounded-sm bg-[#15803D] text-white w-full text-left cursor-pointer">All Trees</button>';
-    
-    categories.forEach((category) => {
-  
-      const div = document.createElement("div");
-      div.innerHTML = `
+  const url = "https://openapi.programming-hero.com/api/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAllCategory(data.categories));
+};
+
+const showAllCategory = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  categoryContainer.innerHTML = " ";
+  categoryContainer.innerHTML =
+    '<button onclick="allPlants()" class="h-[40px] py-2 px-4 rounded-sm bg-[#15803D] text-white w-full text-left cursor-pointer">All Trees</button>';
+
+  categories.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
       <button onclick="categoryPlant(${category.id})" class="h-[40px] py-2 px-4 rounded-sm  w-full text-left cursor-pointer">${category.category_name}</button>`;
-      categoryContainer.appendChild(div);
-    });
-  };
-  
-  
-  // plants by categories 
-  
-  const categoryPlant = (id)=>{
-    const url = `https://openapi.programming-hero.com/api/category/${id}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => showCategoryPlant(data.plants))
-}
+    categoryContainer.appendChild(div);
+  });
+};
 
-const showCategoryPlant = (plants)=>{
-    const plantsContainer = document.getElementById("plants-container");
-    plantsContainer.innerHTML="";
+// plants by categories
 
-    plants.forEach(plant =>{
-        const div = document.createElement('div');
-        div.innerHTML = `<div class="p-4 flex flex-col justify-around items-start space-y-5 bg-white rounded-lg h-full">
+const categoryPlant = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showCategoryPlant(data.plants));
+};
+
+const showCategoryPlant = (plants) => {
+  const plantsContainer = document.getElementById("plants-container");
+  plantsContainer.innerHTML = "";
+
+  plants.forEach((plant) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="p-4 flex flex-col justify-around items-start space-y-5 bg-white rounded-lg h-full">
 
                     <div  class="w-[320px] h-[186px] rounded-md bg-[url(${plant.image})] bg-cover bg-center"></div>
                     
@@ -76,24 +74,27 @@ const showCategoryPlant = (plants)=>{
                         <p class="font-bold">৳${plant.price}</p>
                     </div>
                     <button class="btn text-white bg-[#15803D] w-full">Add to Cart</button>
-                </div>`
-        plantsContainer.appendChild(div);
-    })
-}
+                </div>`;
+    plantsContainer.appendChild(div);
+  });
+};
 
-const loadPlantDetails = (id)=>{
-    const url = `https://openapi.programming-hero.com/api/plant/${id}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
+const loadPlantDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showPlantDetails(data.plants));
+};
 
-  const showPlantDetails = ()=>{
+const showPlantDetails = (plant) => {
+  console.log(plant);
+  const plantDetailContainer = document.getElementById("plant-detail-container");
+  plantDetailContainer.innerHTML = `<h2 class="text-2xl font-bold">${plant.name}</h2>
+                    <div class="bg-[url(${plant.image})] h-[200px] w-full rounded-sm bg-cover bg-center"></div>
+                    <p ><span class="font-semibold text-xl">Category:</span> ${plant.category}</p>
+                    <p ><span class="font-bold">Price:</span> ৳${plant.price}</p>
+                    <p> <span class="font-bold">Description:</span> ${plant.description}</p>`
+  document.getElementById("plant_modal").showModal();
+};
 
-  }
-  
-  
-  
-  
-  
-  allCategories();
+allCategories();

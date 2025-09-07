4e08cc1854ee2600d3f4d6868f0e7d3eee71cd88
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-09-07 16:49:01 +0800

    added interactivity on category btn and show same category plants when click btn

--- script.js
+++ script.js
@@ -50,6 +50,35 @@ const allCategories = () => {
   };
   
   
+  // plants by categories 
+  
+  const categoryPlant = (id)=>{
+    const url = `https://openapi.programming-hero.com/api/category/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCategoryPlant(data.plants))
+}
+
+const showCategoryPlant = (plants)=>{
+    const plantsContainer = document.getElementById("plants-container");
+    plantsContainer.innerHTML="";
+
+    plants.forEach(plant =>{
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="p-4 flex flex-col justify-around items-start space-y-5 bg-white rounded-lg h-full">
+                    <img src="${plant.image}" alt="" class="w-[318px] h-[186px] rounded-md">
+                    <h2 class="text-[14px] font-bold">${plant.name}</h2>
+                    <p class="text-[12px]">${plant.description}</p>
+                    <div class="text-[14px] flex flex-row justify-between w-full h-[28px] items-center">
+                        <button class="btn bg-[#DCFCE7] text-[#15803D] rounded-3xl">${plant.category}</button>
+                        <p class="font-bold">৳${plant.price}</p>
+                    </div>
+                    <button class="btn text-white bg-[#15803D] w-full">Add to Cart</button>
+                </div>`
+        plantsContainer.appendChild(div);
+    })
+}
+
 
   
   
